74361fa05b12c61a7d7bcd16db0873f73f5697e2
Flavio Moreira Terra <flavio.moreira@venturus.org.br>
2022-08-14 14:34:29 -0300

    feat-retrofit-setup

---
+++ app/src/main/java/com/flavio/newsapp/Source.kt
@@ -0,0 +1,6 @@
+package com.flavio.newsapp
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/flavio/newsapp/api/NewsAPI.kt
@@ -0,0 +1,24 @@
+package com.flavio.newsapp.api
+
+import com.flavio.newsapp.util.Constants.Companion.API_KEY
+import com.flavio.newsapp.NewsResponse
+import retrofit2.http.Query
+import retrofit2.http.GET
+import retrofit2.Response
+
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country") countryCode: String = "br",
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q") searchQuery: String,
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<NewsResponse>
+}
