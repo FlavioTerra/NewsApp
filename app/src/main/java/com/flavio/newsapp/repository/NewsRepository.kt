a0b2cd5c6ab48cb2861833689b75b9d197364330
Flavio Moreira Terra <flavio.moreira@venturus.org.br>
2022-09-07 16:58:41 -0300

    feat-setup-search-news-response

--- app/src/main/java/com/flavio/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/flavio/newsapp/repository/NewsRepository.kt
@@ -6,6 +6,9 @@ import com.flavio.newsapp.db.ArticleDatabase
 class NewsRepository(
     val db: ArticleDatabase
 ) {
-    suspend fun getBreakingNews(countryCode: String, page: Int) = RetrofitInstance.api.getBreakingNews(countryCode, page)
+    suspend fun getBreakingNews(countryCode: String, page: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, page)
 
+    suspend fun searchNews(searchQuery: String, page: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, page)
 }
--- app/src/main/java/com/flavio/newsapp/util/Constants.kt
+++ app/src/main/java/com/flavio/newsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "fb5e93bec86c41e9aa9c93fa134d2997"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
